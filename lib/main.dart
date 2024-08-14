adaf0af83021527162f22da5acd4734a997bda7c
farshidalamevan <farshid.a.evan@gmail.com>
2024-08-14 23:42:22 +0600

    Module 10 Live Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-/// Module 10 Assignment
+/// Module 10 Live Test
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Module10Assignment());
+  runApp(Module10LiveTest());
 }
 
-class Module10Assignment extends StatelessWidget {
-  const Module10Assignment({super.key});
+class Module10LiveTest extends StatelessWidget {
+  const Module10LiveTest({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,38 +25,58 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Greeting App'),
+        title: Text('Text Styling App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              'Flutter Text Styling',
+              style: TextStyle(
+                fontSize: 32,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             SizedBox(
-              height: 10,
+              height: 8,
             ),
-            Text('Welcome To Flutter'),
-            Image.network(
-              'https://cdn.ostad.app/public/upload/2024-03-20T04-26-32.799Z-ostad_logo_light@512x%20(2).png',
-              width: 100,
-              height: 100,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.green),
+            Text(
+              'Experiment with text styles',
+              style: TextStyle(
+                fontSize: 16,
+                fontStyle: FontStyle.italic,
               ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextButton(
               onPressed: () {
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
-                    content: Text('Button Pressed!'),
+                    content: Text('You clicked the button'),
                   ),
                 );
               },
-              child: Text('Press Me!'),
+              child: Text('Click Me'),
             ),
+            Text.rich(
+              TextSpan(
+                text: 'Welcome to ',
+                children: [
+                  TextSpan(
+                    text: 'Flutter!',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
